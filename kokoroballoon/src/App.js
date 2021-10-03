646626cf1e7e0e6fc39c07a7d93bdf4ca87c1d1c
Joaquin Gonzalez <joaquingonzalez008@gmail.com>
2021-10-03 18:21:32 -0300

    All routes Createds

--- kokoroballoon/src/App.js
+++ kokoroballoon/src/App.js
@@ -1,14 +1,21 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Login } from './Views/Login/Login'
 import { Principal } from './Views/Principal/Principal'
+import { SignUp } from './Views/SignUp/SignUp'
+import { Profile } from './Views/Profile/Profile'
+import { Lessons } from './Views/Lessons/Lessons'
+import { Lesson } from './Views/Lesson/Lesson'
 
 function App() {
   return (
     <Router>
-
       <Switch>
         <Route path='/' exact component={Principal} />
         <Route path='/login' component={Login} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/lessons' component={Lessons} />
+        <Route path='/lesson' component={Lesson} />
       </Switch>
     </Router>
   );
---
+++ kokoroballoon/src/Views/SignUp/SignUp.js
@@ -0,0 +1,8 @@
+export const SignUp = () => {
+    return (
+        <>
+            <h1>SignUp</h1>
+        </>
+    );
+
+};
